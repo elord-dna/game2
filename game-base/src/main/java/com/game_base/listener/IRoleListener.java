7ef02965ede0f610d2f6e976e8f8a3984fbdb78b
elord_dna <814592344@qq.com>
2017-08-17 15:26:55 +0800

    add some event of role

---
+++ game-base/src/main/java/com/game_base/listener/IRoleListener.java
@@ -0,0 +1,26 @@
+package com.game_base.listener;
+
+import com.game_base.base.Role;
+
+public interface IRoleListener extends IEventListener {
+    // when talk with other role, or even self
+    default void onTalk(Role role) {};
+    // when damaged by other roles
+    default void onDamaged(Role role) {};
+    // when healed
+    default void onHealed(Role role) {};
+    // when scared
+    default void onScared(Role role) {};
+    // when caught
+    default void onCaught(Role role) {};
+    // when stun
+    default void onStun(Role role) {};
+    // when dizzied
+    default void onDizzied(Role role) {};
+    // when charmed
+    default void onCharmed(Role role) {};
+    // when fired
+    default void onFired() {};
+    // when sleepy
+    default void onSleepy() {};
+}
